8d70fedd0d99a3a241234f96e3c6e032e2db7513
ZeroUltra <929288107@qq.com>
2019-01-26 23:36:31 +0800

    zero 1

---
+++ Assets/Scripts/Zero/Role.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Role : MonoBehaviour
+{
+    private Arm arm;
+    private PlayerInfo playerInfo;
+    public void Init()
+    {
+      
+
+    }
+
+}
